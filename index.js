c14d26ce445de6af7056d44bf1d97ed09b68bb2f
skashequr <learningwithextracare@gmail.com>
2023-12-15 17:52:44 +0600

    delited routes done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://resturent-management:iMkmIeA3BMj2mP1N@cluster0.ruyf0su.mongodb.net/?retryWrites=true&w=majority";
 // const uri = `mongodb+srv://resturent-management:$iMkmIeA3BMj2mP1N@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -38,6 +38,12 @@ async function run() {
     })
     app.get("/users",async(req,res)=>{
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+    app.delete("/users/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     })
     app.post('/carts', async(req,res)=>{
