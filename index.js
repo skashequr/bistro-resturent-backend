654da8bf228e751d918582438bebcd6b475b2616
skashequr <learningwithextracare@gmail.com>
2023-12-16 11:25:16 +0600

    added verifyToken middlewares

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-
+const jwt = require('jsonwebtoken');
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -31,12 +31,37 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const booingCollection = client.db("bistroDb").collection("booking");
+
+     // jwt related api
+     app.post('/jwt', async (req, res) => {
+      // 
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+     // middlewares 
+     const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
     app.post("/users",async(req,res)=>{
+      
       const users = req.body;
       const result = await usersCollection.insertOne(users);
       res.send(result);
     })
     app.get("/users",async(req,res)=>{
+      console.log(req.headers);
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
